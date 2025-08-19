75981afe0d1202596c4ce911a8f61b51ff1e954e
Samyabrata roy <samyaroy00@gmail.com>
2025-08-19 12:58:17 +0530

    Credentials Added

--- src/router/index.js
+++ src/router/index.js
@@ -43,7 +43,12 @@ const router = createRouter({
   history: createWebHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
-    // Always scroll to top when navigating
+    if (savedPosition) {
+      return savedPosition
+    }
+    if (to.hash) {
+      return { el: to.hash, behavior: 'smooth' }
+    }
     return { top: 0 }
   }
 })
