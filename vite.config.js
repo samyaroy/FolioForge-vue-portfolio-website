debc3ca578862bebde5a5ed86998739283d48237
Samyabrata roy <samyaroy00@gmail.com>
2025-08-20 11:41:56 +0530

    Footer Templatized

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import yaml from "vite-plugin-yaml2"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), yaml()],
   base: '/',
   resolve: {
     alias: {
