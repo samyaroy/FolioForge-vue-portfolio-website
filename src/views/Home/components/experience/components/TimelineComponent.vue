8f8fc299b564e9ff84a761b977c0916af3795ec5
Samyabrata roy <samyaroy00@gmail.com>
2025-09-26 00:48:36 +0530

    UPdate and Debug

--- src/views/Home/components/experience/components/TimelineComponent.vue
+++ src/views/Home/components/experience/components/TimelineComponent.vue
@@ -9,20 +9,32 @@
 
     <!-- Experience Info -->
     <div class="flex flex-1 flex-col py-3">
-      <p class="text-[#0e141b] text-base font-medium leading-normal">{{ title }} <a v-if="cred_link && cred_link !== '#'" :href="cred_link">
-          <v-icon size="20">mdi-file-document-outline</v-icon>
-        </a>
-      </p>
-      <!-- Time -->
-      <div class="flex items-center gap-2 mt-1">
-        <v-icon class="text-[#4e7397]" size="16">mdi-calendar</v-icon>
-        <p class="text-[#4e7397] text-base font-normal leading-normal">{{ time }}</p>
-      </div>
+      <div class="flex items-center justify-between">
+  <!-- Left side: title + optional link -->
+  <p class="text-[#0e141b] text-base font-medium leading-normal flex items-center gap-1">
+    {{ title }}
+    <a v-if="cred_link && cred_link !== '#'" :href="cred_link" target="_blank">
+      <v-icon size="20">mdi-file-document-outline</v-icon>
+    </a>
+  </p>
+
+  <!-- Right side: time -->
+  <div class="flex items-center gap-1">
+    <!-- <v-icon class="text-[#4e7397]" size="16">mdi-calendar</v-icon> -->
+    <p class="text-[#4e7397] text-base font-normal leading-normal">{{ time }}</p>
+  </div>
+</div>
 
       <!-- Organization -->
       <div class="flex items-center gap-2 mt-1">
         <v-icon class="text-[#4e7397]" size="16">mdi-domain</v-icon>
         <p class="text-[#4e7397] text-sm font-normal leading-normal">{{ organization }}</p>
+      </div>
+
+      <!-- Organization -->
+      <div v-if="supervisor" class="flex items-center gap-2 mt-1">
+        <v-icon class="text-[#4e7397]" size="16">mdi-account-tie</v-icon>
+        <p class="text-[#4e7397] text-sm font-normal leading-normal">{{ supervisor }}</p>
       </div>
 
       <!-- Location -->
@@ -46,6 +58,7 @@ defineProps({
   title: { type: String, required: true },
   time: { type: String, required: true },
   organization: { type: String, required: true },
+  supervisor: { type: String, default: '' },
   location: { type: String, required: true },
   description: { type: String, default: '' },
   icon: { type: String, default: 'mdi-school' }, 
---
+++ src/views/OngoingProjects/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="min-h-screen bg-slate-50 py-8">
+    <div class="container mx-auto px-4">
+      <div class="text-center mb-12">
+        <h1 class="text-4xl font-black text-[#0e141b] mb-4 tracking-[-0.033em]">
+          Ongoing Projects
+        </h1>
+         <p class="text-lg text-gray-600 max-w-4xl mx-auto"> 
+          List of all projects I'm currently working on, including personal, academic, and collaborative projects.
+        </p>
+        </div>
+      <!-- Workshops Section -->
+      <div class="mb-16 max-w-4xl mx-auto">
+        <!-- <h2 class="text-3xl font-bold text-[#0e141b] mb-6">Workshops Attended</h2> -->
+        <!-- <p class="text-gray-600">Academic and professional workshops I've participated in</p> -->
+
+        <div class="space-y-6">
+          <ProjectCard v-for="(project, index) in ongoingProjects" :key="index" :project="project" />
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ProjectCard from './components/ProjectCard.vue'
+
+import config from '@/profile_info.yml'
+const { ongoing_projects } = config
+const ongoingProjects = ongoing_projects || []
+</script>
+
+<style scoped>
+/* Optional styles */
+</style>
