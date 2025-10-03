c6735d6b1944d8244ab2b93d6adee0f43973265e
Samyabrata roy <samyaroy00@gmail.com>
2025-10-04 01:22:30 +0530

    Updated files with Kaggle icon and link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <hr class="footer-divider bg-slate-50" />
-  
+
   <v-footer class="bg-blue-800 text-black py-8">
     <!-- Colored horizontal bar -->
 
@@ -23,6 +23,13 @@
               </v-btn>
               <span class="text-xs text-gray-500 mt-1">Student A/c (IITM)</span>
             </div>
+            <v-btn icon variant="text" color="black" :href=kaggle target="_blank">
+              <v-icon>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+                  <path fill="#1f1f1e" d="M464.2 565.5L318.4 384.3L458.2 249C460.8 246.3 459.9 238.5 452.9 238.5L383.7 238.5C380.2 238.5 376.7 240.3 373.2 243.8L240.9 377.5L240.9 71.5C240.9 66.5 238.4 64 233.4 64L181.5 64C176.5 64 174 66.5 174 71.5L174 568.5C174 573.5 176.5 576 181.5 576L233.4 576C238.4 576 240.9 573.5 240.9 568.5L240.9 459.5L271.7 430.2L382.2 570.8C385.2 574.3 388.7 576.1 392.7 576.1L459.6 576.1C463.1 576.1 465.1 575.1 465.6 573.1L464.2 565.5z"/>
+                </svg>
+              </v-icon>
+            </v-btn>
             <v-btn icon variant="text" color="black" :href=linkedin target="_blank">
               <v-icon>mdi-linkedin</v-icon>
             </v-btn>
@@ -63,18 +70,22 @@
                 Co-curricular
               </router-link>
             </li> -->
-            
+
             <li>
-              <router-link to="/ongoing-projects" class="text-black-300 hover:text-black transition-colors duration-200">
+              <router-link to="/ongoing-projects"
+                class="text-black-300 hover:text-black transition-colors duration-200">
                 Ongoing Projects
-              </router-link></li>
-              <li>
-              <router-link to="/internships-certifications" class="text-black-300 hover:text-black transition-colors duration-200">
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/internships-certifications"
+                class="text-black-300 hover:text-black transition-colors duration-200">
                 Internships & Certifications
               </router-link>
             </li>
             <li>
-              <router-link to="/workshops-bootcamps-attended" class="text-black-300 hover:text-black transition-colors duration-200">
+              <router-link to="/workshops-bootcamps-attended"
+                class="text-black-300 hover:text-black transition-colors duration-200">
                 Bootcamps & Workshops attended
               </router-link>
             </li>
@@ -87,7 +98,7 @@
               </router-link>
             </li>-->
           </ul>
-          <br/>
+          <br />
           <h4 class="text-lg font-semibold mb-2">Social Links</h4>
 
           <ul class="space-y-2">
@@ -97,7 +108,7 @@
             <li>
               <a :href="researchgate" target="_blank">ResearchGate</a>
             </li>
-          </ul> 
+          </ul>
         </div>
 
         <!-- Contact Info -->
@@ -117,9 +128,11 @@
               <span>{{ location }}</span>
             </div>
           </div>
-        <div class="pt-4 pb-0">
-            <a href="https://info.flagcounter.com/Wh9G"><img src="https://s01.flagcounter.com/count2/Wh9G/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_10/viewers_0/labels_0/pageviews_0/flags_0/percent_0/" alt="Flag Counter" border="0"></a>
-        </div>
+          <div class="pt-4 pb-0">
+            <a href="https://info.flagcounter.com/Wh9G"><img
+                src="https://s01.flagcounter.com/count2/Wh9G/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_10/viewers_0/labels_0/pageviews_0/flags_0/percent_0/"
+                alt="Flag Counter" border="0"></a>
+          </div>
         </div>
       </div>
 
@@ -140,10 +153,11 @@
         </div>
       </div>
       <p class="text-gray-500 text-sm">
-          &ensp; Source code available at <a href="https://github.com/samyaroy/FolioForge-vue-portfolio-website/tree/V1_template">GitHub Repo</a>.
-          <br />
-          &ensp; Please fork/clone from there — not directly from the site’s source.
-        </p>
+        &ensp; Source code available at <a
+          href="https://github.com/samyaroy/FolioForge-vue-portfolio-website/tree/V1_template">GitHub Repo</a>.
+        <br />
+        &ensp; Please fork/clone from there — not directly from the site’s source.
+      </p>
 
     </div>
   </v-footer>
@@ -152,7 +166,10 @@
 <script setup>
 import config from "@/profile_info.yml"
 
-const { profile, contacts, socials } = config
+const { profile, contacts, socials } = config;
+
+
+
 // Footer component
 const email = contacts.email
 const phone = contacts.phone
@@ -161,6 +178,7 @@ const location = contacts.location
 const github = socials.github
 const github2 = socials.github2
 const linkedin = socials.linkedin
+const kaggle = socials.kaggle
 //const twitter = socials.twitter
 
 const google_scholar = socials.google_scholar
--- src/main.js
+++ src/main.js
@@ -9,10 +9,15 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import '@mdi/font/css/materialdesignicons.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faKaggle } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faKaggle)
 
 const vuetify = createVuetify({
   components,
   directives,
 })
 
-createApp(App).use(router).use(vuetify).mount('#app')
+createApp(App).use(router).use(vuetify).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
