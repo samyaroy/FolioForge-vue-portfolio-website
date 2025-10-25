45fc2fe840b26d5ed116d35e5ede90b9a13fc3c6
Samyabrata roy <samyaroy00@gmail.com>
2025-10-26 01:12:33 +0530

    Sitemap Generation Added

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,24 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import yaml from '@modyfi/vite-plugin-yaml' 
+import ViteSitemap from 'vite-plugin-sitemap'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), yaml()],
+  plugins: [vue(), yaml(), ViteSitemap({
+      hostname: 'https://samyabrata.codeium.xyz',
+      routes: [
+        '#/',
+        '#/projects-publications',
+        '#/cocurricular',
+        '#/contact',
+        '#/projects-publications?tab=publications',
+        '#/ongoing-projects',
+        '#/internships-certifications'
+      ],
+    })
+  ],
   base: '/',
   resolve: {
     alias: {
