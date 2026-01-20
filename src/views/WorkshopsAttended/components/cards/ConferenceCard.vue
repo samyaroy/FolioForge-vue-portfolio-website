8f0a8c217e477a860f6d1bf2efb066ce1b8d06c4
Samyabrata roy <samyaroy00@gmail.com>
2026-01-20 19:40:33 +0530

    Conference card modified

--- src/views/WorkshopsAttended/components/cards/ConferenceCard.vue
+++ src/views/WorkshopsAttended/components/cards/ConferenceCard.vue
@@ -3,73 +3,96 @@
     class="border-l-4 border-indigo-600 pl-5 py-3 rounded-lg bg-white
            hover:shadow-md transition-all duration-200"
   >
-    <!-- Header -->
-    <div class="flex items-center justify-between mb-1.5 pr-4">
-      <h3 class="text-lg font-semibold text-slate-900">
-        {{ conference.title }}
-        <a
-          v-if="conference.link"
-          :href="conference.link"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="inline-block ml-1 align-middle text-slate-500 hover:text-slate-700"
-        >
-          <v-icon size="16">mdi-open-in-new</v-icon>
-        </a>
-      </h3>
+    <!-- PARENT: flex -->
+    <div class="flex gap-4">
 
-      <span v-if="conference.date" class="text-sm text-gray-500">
-        {{ conference.date }}
-      </span>
-    </div>
+      <!-- CHILD 1: 80% -->
+      <div class="w-[75%] space-y-3">
 
-    <!-- Meta -->
-    <div class="space-y-1">
-
-      <!-- Organizer -->
-      <div
-        v-if="normalizedOrganizers.length"
-        class="text-slate-600 flex items-start"
-      >
-        <v-icon size="small" class="mr-2 mt-0.5">mdi-account-group</v-icon>
-
-        <div class="flex-1 flex">
-          <div class="font-medium mr-2">Organizer:</div>
-
-          <div>
-            <div
-              v-for="(org, index) in normalizedOrganizers"
-              :key="index"
+        <!-- Header -->
+        <div>
+          <h3 class="text-lg font-semibold text-slate-900">
+            {{ conference.title }}
+            <a
+              v-if="conference.link"
+              :href="conference.link"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="inline-block ml-1 align-middle text-slate-500 hover:text-slate-700"
             >
-              <SmartLink :text="org.name" />
-              <span v-if="org.department"> - {{ org.department }}</span>
-              <span v-if="org.location">, {{ org.location }}</span>
+              <v-icon size="16">mdi-open-in-new</v-icon>
+            </a>
+          </h3>
+        </div>
+
+        <!-- Meta -->
+        <div class="space-y-1">
+
+          <!-- Organizer -->
+          <div
+            v-if="normalizedOrganizers.length"
+            class="text-slate-600 flex items-start"
+          >
+            <v-icon size="small" class="mr-2 mt-0.5">mdi-account-group</v-icon>
+
+            <div class="flex-1 flex">
+              <div class="font-medium mr-2">Organizer:</div>
+
+              <div>
+                <div
+                  v-for="(org, index) in normalizedOrganizers"
+                  :key="index"
+                >
+                  <span v-if="org.department">{{ org.department }}, </span>
+                  <SmartLink :text="org.name" />
+                </div>
+              </div>
             </div>
           </div>
+
+          <!-- Institution -->
+          <div
+            v-if="conference.institution?.length"
+            class="text-slate-600 flex items-start"
+          >
+            <v-icon size="small" class="mr-2 mt-0.5">mdi-school</v-icon>
+
+            <div class="flex-1 flex">
+              <div class="font-medium mr-2">Institution:</div>
+
+              <div>
+                <div
+                  v-for="(inst, index) in conference.institution"
+                  :key="index"
+                >
+                  <SmartLink :text="inst.name" />
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
 
-      <!-- Institution -->
-      <div
-        v-if="conference.institution?.length"
-        class="text-slate-600 flex items-start"
-      >
-        <v-icon size="small" class="mr-2 mt-0.5">mdi-school</v-icon>
+      <!-- CHILD 2: 20% -->
+      <div class="w-[20%] text-right text-sm text-gray-500 space-y-1">
 
-        <div class="flex-1 flex">
-          <div class="font-medium mr-2">Institution:</div>
-
-          <div>
-            <div
-              v-for="(inst, index) in conference.institution"
-              :key="index"
-            >
-              <SmartLink :text="inst.name" />
-              <span v-if="inst.department"> - {{ inst.department }}</span>
-              <span v-if="inst.location">, {{ inst.location }}</span>
-            </div>
-          </div>
+        <div
+          v-if="conference.date"
+          class="flex items-center justify-end gap-1"
+        >
+          <v-icon size=14>mdi-calendar</v-icon>
+          <span>{{ conference.date }}</span>
         </div>
+
+        <div
+          v-if="conference.location"
+          class="flex items-center justify-end gap-1"
+        >
+          <v-icon size=14>mdi-map-marker</v-icon>
+          <span>{{ conference.location }}</span>
+        </div>
+
       </div>
 
     </div>
