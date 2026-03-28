3fda03eed06bd9665ae516a4a6da93f358fcb23d
Samyabrata roy <samyaroy00@gmail.com>
2026-03-29 00:48:34 +0530

    Fix affiliation display logic and update message in AffiliationsTab component

--- src/views/Affilications/components/tabs/AffiliationsTab.vue
+++ src/views/Affilications/components/tabs/AffiliationsTab.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="space-y-4">
     <div
-      v-if="!affiliations.length"
+      v-if="!affiliationItems.length"
       class="border border-dashed border-slate-300 rounded-lg p-4 text-center text-slate-500 bg-white"
     >
-      No current affiliation available.
+      No affiliations listed yet
     </div>
 
     <AffiliationCard
-      v-for="(affiliation, index) in affiliations"
+      v-for="(affiliation, index) in affiliationItems"
       :key="index"
       :affiliation="affiliation"
     />
@@ -16,13 +16,17 @@
 </template>
 
 <script setup>
+import { computed } from 'vue'
 import AffiliationCard from '../cards/AffiliationCard.vue'
 
-defineProps({
+const props = defineProps({
   affiliations: {
     type: Array,
     default: () => [],
   },
 })
-</script>
 
+const affiliationItems = computed(() => (
+  Array.isArray(props.affiliations) ? props.affiliations : []
+))
+</script>
