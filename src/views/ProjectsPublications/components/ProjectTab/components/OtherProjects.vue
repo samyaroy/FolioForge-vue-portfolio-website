2d951cf3bfbd07d36b1a57a745aaacd61116ff5f
Samyabrata roy <samyaroy00@gmail.com>
2026-04-14 23:14:11 +0530

    Enhance hyperlink handling by adding aliases for Indian Institute of Technology Madras; update affiliation and host references in Leadership, Volunteering, and OtherProjects components to use SmartLink for improved link management.

--- src/views/ProjectsPublications/components/ProjectTab/components/OtherProjects.vue
+++ src/views/ProjectsPublications/components/ProjectTab/components/OtherProjects.vue
@@ -19,7 +19,7 @@
                         <div v-if="project.affiliation" class="flex items-start gap-2 mb-2">
                             <v-icon class="text-gray-600">mdi-school</v-icon>
                             <p class="text-gray-600">
-                                {{ project.affiliation }}
+                                <SmartLink :type="'Institution'" :text="project.affiliation" />
                             </p>
                         </div>
 
