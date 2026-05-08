8a65977235fed6dce467c5f5890cb00908e6d16e
Samyabrata roy <samyaroy00@gmail.com>
2026-05-09 02:12:00 +0530

    Refactor code structure for improved readability and maintainability

--- src/components/Footer/Index.vue
+++ src/components/Footer/Index.vue
@@ -236,7 +236,9 @@ export default {
       google_scholar: socials.google_scholar,
       researchgate: socials.researchgate,
       // logos: ['MSRKAV', 'NN', 'SNU', 'IITM', 'IDEAS'],
-      logos: ['SNU', 'IITM', 'IDEAS'],
+      logos: ['SNU', 'IITM', 'IDEAS-ISI'],
+      // logos: ['SNU', 'IITM', 'IDEAS'],
+
       last_updated_on: config.last_updated_on,
       betaVersionUrl: profile.beta_version_url,
       showOngoingProjectsLink,
