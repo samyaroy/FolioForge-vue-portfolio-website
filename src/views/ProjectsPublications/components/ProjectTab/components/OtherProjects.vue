b9f66ad899dde0c8d4122e7006e76a5d22798d1f
Samyabrata Roy <samyaroy00@gmail.com>
2026-06-01 04:47:24 +0530

    Add new logos for institutes and enhance project collaborator display in OtherProjects component

--- src/views/ProjectsPublications/components/ProjectTab/components/OtherProjects.vue
+++ src/views/ProjectsPublications/components/ProjectTab/components/OtherProjects.vue
@@ -39,7 +39,23 @@
                             </div>
                         </div>
 
-
+                        <div v-if="getCollaborators(project.collaborators).length" class="flex items-start gap-2 mb-2">
+                            <div class="text-gray-600 flex items-start shrink-0">
+                                <v-icon class="mr-1">mdi-account-supervisor</v-icon>
+                                Collaborators:
+                            </div>
+                            <div class="text-gray-600">
+                                <template v-for="(collaborator, collaboratorIndex) in getCollaborators(project.collaborators)"
+                                    :key="collaborator.key">
+                                    <SmartLink
+                                        :type="'Person'"
+                                        :text="collaborator.name"
+                                        :href="collaborator.link"
+                                    />
+                                    <span class="mr-2" v-if="collaboratorIndex < getCollaborators(project.collaborators).length - 1">,</span>
+                                </template>
+                            </div>
+                        </div>
 
                         <p class="content-justify text-gray-700 mb-3">
                             {{ project.description }}
@@ -54,9 +70,9 @@
                     </div>
 
                     <div
-                        class="w-full md:w-[5%] flex flex-col items-start justify-start gap-3 md:items-center mt-4 md:mt-0">
+                        class="w-full md:w-[7%] flex flex-col items-start justify-start gap-3 md:items-center mr-2 mt-4 md:mt-0">
                         <img v-for="logo in getLogos(project.logo)" :key="logo" :src="getLogoPath(logo)" :alt="logo"
-                            :title="logo" class="max-h-8 md:max-h-12 object-contain opacity-90">
+                            :title="logo" class="max-h-8 md:max-h-10 object-contain opacity-90">
                     </div>
                 </div>
 
@@ -136,6 +152,43 @@ function getLogos(logo) {
     }
 
     return []
+}
+
+function getCollaborators(collaborators) {
+    if (Array.isArray(collaborators)) {
+        return collaborators
+            .map((collaborator, index) => {
+                if (typeof collaborator === 'string') {
+                    return createCollaborator(collaborator, null, index)
+                }
+
+                return createCollaborator(
+                    collaborator?.name,
+                    collaborator?.link || collaborator?.website || collaborator?.url || null,
+                    index
+                )
+            })
+            .filter(collaborator => collaborator.name)
+    }
+
+    if (typeof collaborators === 'string') {
+        return collaborators
+            .split(',')
+            .map((name, index) => createCollaborator(name, null, index))
+            .filter(collaborator => collaborator.name)
+    }
+
+    return []
+}
+
+function createCollaborator(name, link, index) {
+    const trimmedName = typeof name === 'string' ? name.trim() : ''
+
+    return {
+        name: trimmedName,
+        link,
+        key: `${trimmedName}-${index}`,
+    }
 }
 
 function hasActionLinks(project) {
